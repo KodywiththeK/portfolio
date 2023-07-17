afcd6b235cf88e6b62be40f3124a762c7315a4ce
KodywiththeK <jmsds.km@gmail.com>
2023-07-18 05:11:38 +0900

    fixed: sanity.config

--- sanity-studio/sanity.config.ts
+++ sanity-studio/sanity.config.ts
@@ -2,9 +2,6 @@ import {defineConfig, isDev} from 'sanity'
 import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
-import {getStartedPlugin} from './plugins/sanity-plugin-tutorial'
-
-const devOnlyPlugins = [getStartedPlugin()]
 
 export default defineConfig({
   name: 'default',
@@ -13,7 +10,7 @@ export default defineConfig({
   projectId: '3j90b09f',
   dataset: 'production',
 
-  plugins: [deskTool(), visionTool(), ...(isDev ? devOnlyPlugins : [])],
+  plugins: [deskTool(), visionTool()],
 
   schema: {
     types: schemaTypes,
